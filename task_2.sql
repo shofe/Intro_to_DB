9d0a20b72e73d0cfc4c818187927d7716f110a74
shofe <tayoshofunlayo@gmail.com>
2025-07-25 14:33:40 +0100

    Made a change from the Orders Table, the customers was wrongly written

--- task_2.sql
+++ task_2.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
@@ -34,5 +34,5 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     book_id INT,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
